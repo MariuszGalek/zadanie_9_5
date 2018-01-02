077e9ec982cc58ea5bedd0ced58a2a503e28a729
Mariusz Galek <mgalek1@gmail.com>
2018-01-03 00:16:56 +0100

    Add DOM elements

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+// scripts.js
+
+var withButtonClass = document.getElementsByClassName('button');
+
+console.log(withButtonClass);
+
+for (var i = 0; i < withButtonClass.length; i++) {
+		console.log(withButtonClass[i].innerText);
+}
